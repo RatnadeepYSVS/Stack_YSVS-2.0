be2126e5a78583479af0a63dacc9933ee7e74704
RATNADEEP YSVS <68638971+RatnadeepYSVS@users.noreply.github.com>
2021-02-19 22:50:31 +0530

    Darkmode Enabled Script.JS file

--- script.js
+++ script.js
@@ -1,15 +1,9 @@
-let city,input,btn,bdy=document.body,checker=false
+let city,input,btn,bdy=document.body,checker=document.querySelector('input')
 bdy.style.fontFamily='Lato,Sans-seriff'
 bdy.style.backgroundColor='#FB6612'
 bdy.style.textAlign='center'
 const add=(c)=>bdy.append(c)
 const create=(c)=>document.createElement(c)
-const imags=create('img')
-imags.src='https://icons-for-free.com/iconfiles/png/512/fog+foggy+weather+icon-1320196634851598977.png'
-imags.style.width='25%'
-imags.style.display='block'
-imags.style.margin='auto'
-add(imags)
 const main=create('h1')
 main.innerHTML='Forecast.'
 main.style.marginTop='-2vh'
@@ -38,8 +32,35 @@ btn.style.padding='7px 10px'
 btn.style.background='black'
 btn.style.color='white'
 btn.style.textDecoration='none'
+btn.style.transition='all 2s ease-in-out'
 add(btn)
+localStorage.setItem('mode',checker.checked)
 const setCity=()=>{
     localStorage.setItem('city',input.value)
 }
+const clickMe =()=>{
+    const mode= localStorage.getItem('mode')
+    const color=checker.checked?'black':'#FB6612'
+    const back=!checker.checked?'black':'white'
+    btn.style.backgroundColor=back
+    bdy.style.background=color
+    bdy.style.color=back
+    const imags=document.querySelector('img')
+    if(checker.checked){
+        imags.src='https://www.wmcactionnews5.com/pb/resources/images/weather/weather-condition-icons/400x400/102_daily_forecast.png'
+        imags.style.width='25%'
+        imags.style.display='block'
+        imags.style.margin='auto'
+        btn.style.color=color
+    }
+    else{
+        imags.src='https://icons-for-free.com/iconfiles/png/512/fog+foggy+weather+icon-1320196634851598977.png'
+        imags.style.width='25%'
+        imags.style.display='block'
+        imags.style.margin='auto'
+        btn.style.color='white'
+    }
+    bdy.style.transition='all 2s ease-in-out'
+}
+checker.addEventListener('click',clickMe)
 btn.addEventListener('click',setCity)
